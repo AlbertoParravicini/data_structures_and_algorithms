6b10d14f8e4f05f15f4350f44023a097d868fcef
Alberto Parravicini <albyratta@gmail.com>
2016-11-23 19:08:59 +0100

    Updated 2d hull

--- Project/convex_hull.py
+++ Project/convex_hull.py
@@ -2,17 +2,19 @@ import numpy as np
 import scipy.spatial as spatial
 import sys
 import matplotlib.pyplot as plt
+import functools
 
 MIN_VALUE = -1000000
 
+
 class Point:
-    def __init__(self, x = 0, y = 0):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
     def __str__(self):
         return "Point(%.2f, %.2f)" % (self.x, self.y)
-    
+
     def __repr__(self):
         return "Point(%.2f, %.2f)" % (self.x, self.y)
 
@@ -20,14 +22,17 @@ class Point:
         if isinstance(other, self.__class__):
             return self.x == other.x and self.y == other.y
         return NotImplemented
-    
+
     def __ne__(self, other):
         if isinstance(other, self.__class__):
             return not self.__eq__(other)
         return NotImplemented
+
     
+
     def to_array(self):
         return [self.x, self.y]
+
 
 # Taken from http://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
 def chunks(list, size):
@@ -35,8 +40,10 @@ def chunks(list, size):
     for i in range(0, len(list), size):
         yield list[i:i + size]
 
+
 def squared_distance(a, b):
-  return (a.x - b.x) ** 2 + (a.y - b.y) ** 2
+    return (a.x - b.x)**2 + (a.y - b.y)**2
+
 
 ###################################
 # ORIENTATION DETERMINANT #########
@@ -52,6 +59,12 @@ def orientation_test(p, q, r):
     #console.log "det:", determinant, "\n"
     return determinant
 
+
+###################################
+# BINARY SEARCH  ##################
+###################################
+
+
 #Default comparator used in binary search.
 def default_comparator(points, value, middle):
     if points[middle] < value:
@@ -59,12 +72,15 @@ def default_comparator(points, value, middle):
     elif points[middle] > value:
         return -1
     else:
-         return 0
+        return 0
+
+
 # Comparator used in binary search to test the point inclusion in a convex polygon.
 def inclusion_comparator(points, query_point, middle):
     p_0 = points[0]
     o_1 = orientation_test(p_0, points[middle], query_point)
-    o_2 = orientation_test(p_0, points[middle + 1], query_point)
+    o_2 = orientation_test(p_0, points[(middle + 1) % len(points)],
+                           query_point)
     if o_1 >= 0 and o_2 >= 0:
         return 1
     elif (o_1 <= 0 and o_2 <= 0) or (o_1 <= 0 and o_2 >= 0):
@@ -72,49 +88,98 @@ def inclusion_comparator(points, query_point, middle):
     else:
         return 0
 
+
+# Comparator used by find_intersecting_edge        
+def intersection_comparator(points, query_point, middle):
+    p_0 = points[0]
+    o_1 = orientation_test(query_point, p_0, points[middle])
+    o_2 = orientation_test(query_point, p_0, points[(middle + 1) %
+                                                    len(points)])
+
+    if (query_point.x <= p_0.x):
+        if o_1 >= 0 and o_2 >= 0:
+            return -1
+        elif o_2 <= 0:
+            return 1
+        else:
+            return 0
+    else:
+        if o_1 >= 0 and o_2 >= 0:
+            return 1
+        elif o_1 <= 0:
+            return -1
+        else:
+            return 0
+
+
+# Comparator used by the binary search to find the bottom tangent.
+def bot_tangent_comparator(points, query_point, middle):
+    o_1 = orientation_test(query_point, points[(middle - 1) % len(points)],
+                           points[middle])
+    o_2 = orientation_test(query_point, points[middle], points[(middle + 1) %
+                                                               len(points)])
+
+    if o_1 * o_2 <= 0:
+        return 0
+    elif o_1 <= 0 and o_2 <= 0:
+        return 1 
+    else:
+        return -1 
+
+
 # Generic implementation of binary search. 
 # It returns the position of the found element, or False if not found. 
-def binary_search(points, value, comparator=default_comparator):
-    
-    start = 0
-    stop = len(points) - 1
+def binary_search(points, value, comparator=default_comparator, force_return = False, start = 0, stop = None):
+    if stop == None:
+        stop = len(points) - 1
     while start < stop:
         middle = (start + stop) // 2
-        if comparator(points, value, middle) > 0:
+        res = comparator(points, value, middle)
+        if res > 0:
             start = middle + 1
-        elif comparator(points, value, middle) < 0:
+        elif res < 0:
             stop = middle
         else:
-             return middle
-    return False
+            return middle
+    return False if not force_return else (start + stop) // 2
+
 
 ##################################
 # POINT INCLUSION IN HULL #########
 ###################################
+
 
 # Find if a given point is inside a convex polygon.
 # Note that the input list of points must be convex,
 # and must be radially sorted counter-clockwise, 
 # starting from the point with smallest x
 def inclusion_in_hull(points, query_point):
-  # pick the first point p0 of the hull as reference
-  # pick the start and stop indexes
-  # pick the middle index 
-  # if  p0-q-middle is left and p0-q-middle+1 is right, stop
-  # if both are left, q is below:
-  #   stop = middle - 1,
-  #   middle = ...
-  # else q is above
-  middle = binary_search(points, query_point, inclusion_comparator)
-  return orientation_test(points[middle], points[middle + 1], query_point) > 0 if middle else False
+    # pick the first point p0 of the hull as reference
+    # pick the start and stop indexes
+    # pick the middle index 
+    # if  p0-q-middle is left and p0-q-middle+1 is right, stop
+    # if both are left, q is below:
+    #   stop = middle - 1,
+    #   middle = ...
+    # else q is above
+    middle = binary_search(points, query_point, inclusion_comparator)
+    return orientation_test(points[middle], points[middle + 1],
+                            query_point) > 0 if middle else False
 
 
+# Given a convex polygon and an external point, find the edge of the polygon that intersect the line 
+# given by the external point and the leftmost point of the polygon.
+def find_intersecting_edge(points, query_point):
+    if inclusion_in_hull(points, query_point):
+        return False
 
-
-
-
-
-
+    middle = binary_search(points, query_point, intersection_comparator)
+    if middle:
+        return middle
+    elif query_point.y < points[0].y:
+        return len(points) - 1
+    else:
+        return 0
 
 
 ###################################
@@ -128,12 +193,13 @@ def inclusion_in_hull(points, query_point):
 def find_tangent_bottom(points, query_point):
     if inclusion_in_hull(points, query_point):
         return False
-    
+
     tan_b = points[0]
     tan_index = 0
     for i in range(1, len(points)):
-        e_prev = orientation_test(points[i - 1], points[i], query_point) >= 0
-        e_next = orientation_test(points[i], points[(i + 1) % len(points)], query_point)
+        e_prev = orientation_test(points[i - 1], points[i], query_point)
+        e_next = orientation_test(points[i], points[(i + 1) % len(points)],
+                                  query_point)
         if e_prev == 0:
             tan_b = points[i - 1]
             tan_index = i - 1
@@ -146,11 +212,133 @@ def find_tangent_bottom(points, query_point):
     return {"tan_point": tan_b, "tan_index": tan_index}
 
 
+###################################
+# FIND TANGENTS TO A POLYGON, BINARY SEARCH
+###################################
+# Find the tangents of a point to a given convex polygon.
+# Note that the input list of points must be convex,
+# and must be radially sorted counter-clockwise, 
+# starting from the point with smallest x.
+def find_tangent_bin_search(points, query_point):
+    if inclusion_in_hull(points, query_point):
+        return False
 
+    tan_index = 0
+    # Find where to split the polygon.
+    stop_bottom = find_intersecting_edge(points, query_point)
+
+    if orientation_test(query_point, points[0], points[stop_bottom]) >= 0 and \
+        orientation_test(query_point, points[0], points[(stop_bottom + 1) % len(points)]) <= 0:
+            tan_index = binary_search(points, query_point,
+                              bot_tangent_comparator, True, start = stop_bottom)
+    else:
+        tan_index = binary_search(points, query_point,
+                              bot_tangent_comparator, True, stop = stop_bottom + 1)
+
+    
+    if not tan_index:
+        tan_index = 0
+    return {"tan_point": points[tan_index], "tan_index": tan_index}
+
+
+###################################
+# RADIAL SORT #####################
+###################################
+# Sort clockwise (or counter-clockwise) a list of points w.r.t. a given anchor point.
+# If the points, translated w.r.t. the anchor, span multiple quadrants of the plane,
+# they are sorted by taking the positive x semi-axis as starting point.
+# Inspired by "http://stackoverflow.com/questions/6989100/sort-points-in-clockwise-order"
+def radial_sort(points, anchor = Point(0., 0.), cw = True):
+    def cmp(a, b):
+        if a.x - anchor.x == 0 and a.y - anchor.y == 0 and b.x - anchor.x != 0 and b.y - anchor.y != 0:
+            return -1
+        if a.x - anchor.x != 0 and a.y - anchor.y != 0 and b.x - anchor.x == 0 and b.y - anchor.y == 0:
+            return 1
+        if a.x - anchor.x >= 0 and b.x - anchor.x < 0:
+            return  -1
+        if a.x - anchor.x < 0 and b.x - anchor.x >= 0:
+            return  1
+        if a.x - anchor.x == 0 and b.x - anchor.x == 0:
+            if a.y - anchor.y < 0 and b.y - anchor.y < 0:
+                return 1 if a.y < b.y else -1
+            else:
+                return 1 if a.y > b.y else -1
+        # Check the position of b wrt the line defined by the anchor and a.
+        # b has greater angle than a if it lies on the left of the line.
+        orientation = orientation_test(anchor, a, b)
+        # if orientation < 0, anchor-a is bigger than anchor-b, as we have a right turn.
+        if orientation == 0:
+            return 1 if squared_distance(anchor, a) >= squared_distance(anchor, b) else -1
+        else: 
+            return 1 if orientation > 0 else -1
+
+
+    if len(points) == 0:
+        return []
+    
+    points.sort(key=functools.cmp_to_key(cmp), reverse = not cw)
+    
+    return points
+
+###################################
+# GRAHAM SCAN #####################
+###################################
+# Compute the convex hull of the given list of points by using Graham scan
+# Inspired by "http://www.geeksforgeeks.org/convex-hull-set-2-graham-scan/"
+def convex_hull_graham_scan(input_points):
+    # Copy the input points, so that it is possible to modify them
+    points = list(input_points)
+    convex_hull = []
+
+    # Find the point with the smalles x.
+    smallest_x_point_index = 0
+    for index, p in enumerate(points):
+        if (p.x < points[smallest_x_point_index].x) or \
+            ((p.x == points[smallest_x_point_index].x) and (p.y < points[smallest_x_point_index].y)):
+            smallest_x_point_index = index
+
+    # Put the point with smallest x at the beginning of the list.
+    points[0], points[smallest_x_point_index] = points[
+        smallest_x_point_index], points[0]
+
+    # Order the list with respect to the angle that each point forms 
+    # with the anchor. Given two points a, b, in the output a is before b
+    # if the polar angle of a w.r.t the anchor is bigger than the one of b,
+    # in counter-clockwise direction.
+    anchor = Point(points[0].x, points[0].y)
+    points = [anchor] + radial_sort(points[1:], anchor, cw = False)
+    # If more points have the same angle w.r.t. the anchor, keep only the farthest one.
+    i = 1
+    while i < len(points) and (orientation_test(anchor, points[i], points[(i + 1) % len(points)]) == 0):
+        points.pop((i + 1) % len(points))
+    
+    # If there are fewer than 3 points, it isn't possible to build a convex hull.
+    if len(points) < 3:
+        raise ValueError("The hull has just ", len(points), ": at least 3 points are needed to build a hull!")
+    
+    # Add the first 3 points to the convex hull.
+    # The first 2 will be for sure part of the hull.
+    convex_hull += points[0:3]
+
+    for p in points[3:]:
+        # While the i-th point forms a non-left turn with the last 2 elements of the convex hull...
+        while orientation_test(convex_hull[-2], convex_hull[-1], p) <= 0:
+            # Delete from the convex hull the point that causes a right turn.
+            convex_hull.pop()
+        # Once no new right turns are found, add the point that gives a left turn.   
+        convex_hull.append(p)
+
+    return convex_hull
+
+
+
+###################################
+# HULL 2D - STEP ##################
+###################################
 def hull_2d_step(points, m, H):
     # Partition the points in groups of size at most m.
     points_in_groups = list(chunks(points, m))
-    
+
     temp_hulls = []
     hulls = []
 
@@ -158,21 +346,10 @@ def hull_2d_step(points, m, H):
 
     # Compute the convex hull of each group, and store its vertices in ccw order.
     for group_i in points_in_groups:
-        array_points = np.reshape([p.to_array() for p in group_i], [-1, 2])
-       
-        hull = array_points[spatial.ConvexHull(array_points).vertices]
-        temp_hulls.append(hull)
+        hulls.append(convex_hull_graham_scan(group_i))
 
-    # TEMPORARY: rebuild points
-    for hull in temp_hulls:
-        temp_hull = []
-        for p in hull:
-            temp_hull.append(Point(p[0], p[1]))
-        hulls.append(temp_hull)
-    
-    final_hull.append(Point(0, MIN_VALUE))
-    # Rigthmost point of the list
-    final_hull.append(Point(MIN_VALUE, MIN_VALUE))
+    # Leftmost point of the list
+    final_hull.append(Point(-MIN_VALUE, -MIN_VALUE))
     # Find the next point, ccw, belonging to the hull of p_1
     p_k = Point(MIN_VALUE, MIN_VALUE)
 
@@ -186,25 +363,25 @@ def hull_2d_step(points, m, H):
     # Hull number of the last point inserted, updated after inserting a new point in the final hull.
     old_hull = -1
 
+    # Find the leftmost point.
     for hull_index, hull_i in enumerate(hulls):
         for i in range(0, len(hull_i)):
-            if hull_i[i].x > final_hull[1].x:
-                final_hull[1] = hull_i[i]
-                current_hull_number = hull_index 
+            if hull_i[i].x < final_hull[0].x:
+                final_hull[0] = hull_i[i]
+                current_hull_number = hull_index
                 pos_in_last_hull = i
 
-    # Find the next point, ccw, belonging to the hull of p_1
-
-
     for k in range(0, H):
-        p_k = hulls[current_hull_number][(pos_in_last_hull + 1) % len(hulls[current_hull_number])]
-        old_hull = current_hull_number
-        # compute the next point in the hull of hull[-1]
+        # Compute the next point in the hull of hull[-1],
         # also store the index of that hull
+        p_k = hulls[current_hull_number][(pos_in_last_hull + 1) %
+                                         len(hulls[current_hull_number])]
+        old_hull = current_hull_number
+
         for hull_index, hull_i in enumerate(hulls):
             # Find the bottom tangent to from p_k-1, p_k to a point q_i in hull_i
-            if hull_index != current_hull_number:
-                temp_p = find_tangent_bottom(hull_i, final_hull[-1])
+            if hull_index != old_hull:
+                temp_p = find_tangent_bin_search(hull_i, final_hull[-1])
                 if temp_p:
                     temp_tan = temp_p["tan_point"]
                     temp_tan_index = temp_p["tan_index"]
@@ -217,15 +394,16 @@ def hull_2d_step(points, m, H):
                     if o > 0:
                         p_k = temp_tan
                         current_hull_number = hull_index
-                        pos_in_last_hull = temp_tan_index                  
+                        pos_in_last_hull = temp_tan_index
         if old_hull == current_hull_number:
-            pos_in_last_hull = (pos_in_last_hull + 1) % len(hulls[current_hull_number])
-        if p_k == final_hull[1]:
-            return final_hull[1:]
+            pos_in_last_hull = (
+                pos_in_last_hull + 1) % len(hulls[current_hull_number])
+        if p_k == final_hull[0]:
+            return final_hull
         final_hull.append(p_k)
-    
+
     return False
-  
+
 
 def hull_2d(points):
     t = 1
--- Project/main.py
+++ Project/main.py
@@ -6,23 +6,20 @@ import pprint
 from convex_hull import *
 
 def main():
-    num_points = 12000
-    m = 4
-    H = 8
-
-    np.random.seed(1020)
-
-
+    num_points = 12
+    #np.random.seed(47516)
+    seed = int(np.floor(np.random.rand() * 100000))
+    print("\n\nSEED: ", seed, "\n\n")
+    np.random.seed(seed)
 
     points = []
-
     for i in range(0, num_points):
-        points.append(Point(np.floor(np.random.rand() * 1500), np.floor(np.random.rand() * 1500)))
+        #points.append(Point(np.floor((np.random.rand() * 20)), np.floor((np.random.rand() * 20))))
+        points.append(Point((np.random.rand() * 20), (np.random.rand() * 20)))
     print("\n\nPOINTS\n")
-    #print(points, end='\n\n')
+    pprint.pprint(points)
 
-    # Test the tangents
-    print("TESTING TANGENTS")
+   
     points_matrix = np.reshape([p.to_array() for p in points], [-1, 2])
     temp_hull = points_matrix[spatial.ConvexHull(points_matrix).vertices]
 
@@ -34,13 +31,26 @@ def main():
     plt.plot(points_matrix[:, 0], points_matrix[:, 1], "o")
     plt.plot(temp_hull[:, 0], temp_hull[:, 1], color='#6699cc', alpha=0.7,
         linewidth=3, solid_capstyle='round', zorder=2)
-    plt.axis([0, 18, 0, 18])
+    plt.axis([0, 22, 0, 22])
 
     #plt.show()
+    print("\n\nGRAHAM SCAN\n")
+    pprint.pprint(convex_hull_graham_scan(points))
+    print("LEN:", len(convex_hull_graham_scan(points)))
     print("\n\nHULL FIGO:\n")
-    print(len(hull_2d(points)))
+    pprint.pprint((hull_2d(points)))
+    print("LEN:", len(hull_2d(points)))
     print("\n\nHULL CORRETTO:\n")
+    print((temp_hull))
     print(len(temp_hull))
 
 
+    # points = [     
+    #     Point(1.66, 15.76),
+    #     Point(17.93, 4.27),
+    #      Point(9.44, 19.60),
+    #     Point(5.26, 19.20)
+    # ]
+    # query_point = Point(19.94, 6.89)
+    # print(find_tangent_bin_search(points, query_point))
 main()
